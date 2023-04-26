8d1ff3d170db4ccbcc98336f16f63637ab252558
Yuneidy Castillo <yuneidycastillo25@gmail.com>
2023-04-26 13:54:34 +0100

    Add projects images and styles

--- script.js
+++ script.js
@@ -31,3 +31,37 @@ function addSocialMedia() {
         image.classList = 'nav__social-media__img';
     }
 }
+
+addProjects();
+
+function addProjects() {
+    const projectList = document.getElementsByClassName('project__list')[0];
+    const projectContainer = document.createElement('div');
+    projectList.appendChild(projectContainer);
+    projectContainer.classList = 'project__list__container';
+
+    const projects = document.createElement('ul');
+    projectContainer.appendChild(projects);
+    projects.classList = 'project__list__items';
+
+    const projectArray = [
+        {image: './assets/image/todo.PNG', link: 'https://github.com/YuneidyC', alt: `Todo's app`},
+        {image: './assets/image/weather.PNG', link: 'https://github.com/YuneidyC', alt: 'Weather'},
+        {image: './assets/image/timer.PNG', link: 'https://github.com/YuneidyC', alt: 'Timer'},
+        {image: './assets/image/batatabit.PNG', link: 'https://github.com/YuneidyC', alt: 'Batatabit'},
+        {image: './assets/image/blog.PNG', link: 'https://github.com/YuneidyC', alt: 'Blog page'},
+        {image: './assets/image/animationLand.PNG', link: 'https://github.com/YuneidyC', alt: 'Animation Land'},
+        {image: './assets/image/restaurantMenu.PNG', link: 'https://github.com/YuneidyC', alt: 'Restaurant page'},
+    ];
+    
+    for(let i = 0; i <= projectArray.length - 1; i++) {
+        let project = document.createElement('li');
+        projects.appendChild(project);
+        project.classList = 'project__list__item';
+
+        let image = document.createElement('img');
+        project.appendChild(image);
+        image.src = projectArray[i].image;
+        image.alt = projectArray[i].alt;
+    }
+}
